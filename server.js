80325cec4090162bbcecd406c704bc31dffafbd7
Ryan Birch <ryan.birch@gmail.com>
2019-06-06 05:42:44 -0400

    completed survey html

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ let PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+app.use(express.static('app/public/assets'))
+
 app.listen(PORT, () => console.log('Listening on port ' + PORT))
 
 
